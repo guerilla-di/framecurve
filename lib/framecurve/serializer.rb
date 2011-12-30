b760210affca693da6cdc6713e9d6c8aaa4082fd
Julik <me@julik.nl>
2011-12-30 10:30:57 +0100

    Comments

--- lib/framecurve/serializer.rb
+++ lib/framecurve/serializer.rb
@@ -1,14 +1,13 @@
+# Writes out a Curve object to the passed IO
 class Framecurve::Serializer
+  # Serialize the passed curve into io. Will use the materialized curve version.
+  # Will write the file with CRLF linebreaks instead of LF
   def serialize(io, curve)
     io.write("# http://framecurve.org/specification-v1\n")
     io.write("# at_frame\tuse_frame_of_source\n")
-    # TODO - lerp instead!
-    curve.each do | element |
-      if element.tuple?
-        io.write("# %s\r\n" % element.text)
-      elsif element.comment?
-        io.write("%d\t%05f\r\n" % [element.at, element.value])
-      end
+    curve.to_materialized_curve.each_tuple do | t |
+      io.write(t)
+      io.write("\r\n")
     end
   end
 end
