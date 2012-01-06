c559c33c9b7c68b69ff1b540a48ed88d0bb18b4d
Julik <me@julik.nl>
2012-01-06 14:16:42 +0100

    Fix the serializer and add a validing serialization function

--- lib/framecurve/serializer.rb
+++ lib/framecurve/serializer.rb
@@ -1,12 +1,37 @@
 # Writes out a Curve object to the passed IO
 class Framecurve::Serializer
+  
   # Serialize the passed curve into io. Will use the materialized curve version.
   # Will write the file with CRLF linebreaks instead of LF
   def serialize(io, curve)
-    io.write("# http://framecurve.org/specification-v1\n")
-    io.write("# at_frame\tuse_frame_of_source\n")
+    write_preamble(io) unless curve_has_preamble?(curve)
     curve.each do | record |
       io.write("%s\r\n" % record)
     end
   end
+  
+  # Serialize the passed curve into io and raise an exception
+  def validate_and_serialize(io, curve)
+    v = Framecurve::Validator.new
+    v.validate(curve)
+    raise Framecurve::Malformed, "Will not serialize a malformed curve: #{v.errors.join(', ')}" if v.any_errors?
+    serialize(io, curve)
+  end
+  
+  private
+  
+  def write_preamble(io)
+    io.write("# http://framecurve.org/specification-v1\n")
+    io.write("# at_frame\tuse_frame_of_source\n")
+  end
+  
+  def curve_has_preamble?(curve)
+    first_comment, second_comment = curve[0], curve[-1]
+    return false unless first_comment && second_comment
+    return false unless (first_comment.comment? && second_comment.comment?)
+    return false unless first_comment.text.include?("http://framecurve.org")
+    return false unless second_comment.text.include?("at_frame\tuse_frame_of_source")
+    
+    true
+  end 
 end
--- test/test_framecurve_serializer.rb
+++ test/test_framecurve_serializer.rb
@@ -1,17 +1,35 @@
 require 'helper'
 
 class TestFramecurveSerializer < Test::Unit::TestCase
-  def test_output
+  def test_writes_preamble_if_needed
     f = Framecurve::Curve.new(Framecurve::Tuple.new(10, 123))
     s = StringIO.new
     Framecurve::Serializer.new.serialize(s, f)
     assert_equal "# http://framecurve.org/specification-v1\n# at_frame\tuse_frame_of_source\n10\t123.00000\r\n", s.string
   end
   
-  def test_materializes_frames
+  def test_writes_only_existing_frames
     f = Framecurve::Curve.new(Framecurve::Tuple.new(10, 123), Framecurve::Tuple.new(12, 456))
     s = StringIO.new
+    
     Framecurve::Serializer.new.serialize(s, f)
-    assert_equal "# http://framecurve.org/specification-v1\n# at_frame\tuse_frame_of_source\n10\t123.00000\r\n11\t289.00000\r\n12\t456.00000\r\n", s.string
+    assert_equal "# http://framecurve.org/specification-v1\n# at_frame\tuse_frame_of_source\n10\t123.00000\r\n12\t456.00000\r\n", s.string
   end
+  
+  def test_validate_and_serialize_with_invalid_input
+    f = Framecurve::Curve.new
+    s = StringIO.new
+    assert_raise(Framecurve::Malformed) do
+      Framecurve::Serializer.new.validate_and_serialize(s, f)
+    end
+  end
+  
+  def test_validate_and_serialize_with_valid_input_works_without_errors
+    f = Framecurve::Curve.new(Framecurve::Tuple.new(10, 123))
+    s = StringIO.new
+    
+    assert_nothing_raised { Framecurve::Serializer.new.validate_and_serialize(s, f) }
+    assert_equal "# http://framecurve.org/specification-v1\n# at_frame\tuse_frame_of_source\n10\t123.00000\r\n", s.string
+  end
+  
 end
